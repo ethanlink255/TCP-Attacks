1dda1f83c0dd7332d2f52a5c760823d7bae2c030
Ethan Link <ethanlink255@gmail.com>
2022-03-18 18:42:08 -0400

    Started Packet Spoofing Code

--- RST/rst.c
+++ RST/rst.c
@@ -9,25 +9,68 @@
 #include <netinet/ip_icmp.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h> //prob unnecessary
-//#include <linux/if_ether.h>  
-#include <netinet/if_ether.h> //MacOS
+#include <linux/if_ether.h>  
+//#include <netinet/if_ether.h> //MacOS
 #include <unistd.h>  
+#include <stdlib.h>
+
 
 void handlePacket(char *buf, int s){
     struct iphdr *ip = (struct iphdr*)(buf + sizeof(struct ethhdr));
     if(ip->protocol == 6){
+        unsigned short hdrlen;
+        hdrlen = ip->ihl * 4;
+
+        struct tcphdr *tcph = (struct tcphdr*)(buf + hdrlen + sizeof(struct ethhdr));
+
+        int seq_num = tcph->seq;
+
+        int sock;
+
+        if((sock = socket(PF_INET, SOCK_RAW, IPPROTO_TCP)) == -1) {
+            perror("Failed to Create Socket");
+            exit(4);
+        }
+
+        char datagram[4096];
+        memset(datagram, 0, 4096);
+
+        struct iphdr *ret = (struct iphdr*) datagram;
         
+        struct tcphdr *spoof = (struct tchhdr*) (datagram + sizeof(stuct ip));
+        struct sockaddr_in sin;
+        struct pseudo_header psh;
+
+        ret->ihl = 5;
+        ret->version = 4;
+        ret->tos = 0;
+        ret->tot_len = sizeof(strcut iphdr) + sizeof(struct tcphdr);
+        ret->id = htonl(28313); //prob doesn't matter
+        ret->frag_off = 0;
+        ret->ttl = 255; //max
+        ret->protocol = IPPROTO_TCP;
+        ret->check = 0;
+        ret->saddr = ip->daddr;
+        ret->daddr = ip->saddr;
+
+
+        struct iphdr spoof;
+        spoof.dest = inet_pton(ip->src);
+        spoof.src = inet_pton(ip->dest);
+    
+
+
     }
 }
 
 int main(){
     struct sockaddr addr;
-    int sock, n;
+    int sock;
     unsigned char *buffer = malloc(65536);
     char *eth_name = "";
 
     struct ifreq ethreq;
-
+ 
     if((sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)) < 0)){
         perror("Socket Creation Failed");
         return 1;
